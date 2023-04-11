b6ebdc4884df8f18fce20a0a0b7d189bf5f0cd8b
patrickgiry <patrickgiry@gmail.com>
2023-04-11 23:11:38 +0200

    Init kata with spring boot

---
+++ src/main/java/katas/bankkata/SpringBootBankKataApplication.java
@@ -0,0 +1,13 @@
+package katas.bankkata;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootBankKataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootBankKataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/katas/bankkata/SpringBootBankKataApplicationTests.java
@@ -0,0 +1,13 @@
+package katas.bankkata;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootBankKataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
