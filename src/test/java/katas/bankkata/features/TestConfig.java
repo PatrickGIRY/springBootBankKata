5be98c289255a71dd423220ec7e370fe0a88bf49
patrickgiry <patrickgiry@gmail.com>
2023-04-18 22:25:19 +0200

    Add test Data source

--- src/test/java/katas/bankkata/features/TestConfig.java
+++ src/test/java/katas/bankkata/features/TestConfig.java
@@ -1,7 +1,22 @@
 package katas.bankkata.features;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+
+import javax.sql.DataSource;
 
 @TestConfiguration
 public class TestConfig {
+
+    @Bean
+    public DataSource dataSource() {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setPoolName("test-db");
+        hikariConfig.setDriverClassName("org.h2.Driver");
+        hikariConfig.setJdbcUrl("jdbc:h2:mem:;MODE=PostgreSQL");
+
+        return new HikariDataSource(hikariConfig);
+    }
 }
